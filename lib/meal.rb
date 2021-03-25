b183313a47f753cd1635a701a99d0f804ff37b2a
Sam D <samidiaz3@gmail.com>
2021-03-25 01:01:27 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -4,9 +4,10 @@ class Meal
 
     @@all = []
 
-    def initialize(waiter, customer, tip = 0)
+    def initialize(waiter, customer, total, tip = 0)
       @waiter = waiter
       @customer = customer
+      @total = total
       @tip = tip
       @@all << self
     end
