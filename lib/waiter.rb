cbb52c54be5fded14e4a7a9f9ab7c43bb78362cd
Sam D <samidiaz3@gmail.com>
2021-03-25 00:57:00 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,15 @@
 class Waiter
 
+  attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize(name, yrs_experience)
+      @name = name
+      @yrs_experience = yrs_experience
+      @@all << self
+    end
+
+
+
 end
